efac762df12ce5b1680566a48c9f870598a65c18
Lukasz Sagol <lukasz@sagol.pl>
2015-03-26 17:37:42 +0000

    Setting up stylesheets from node_modules + bourbon

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bourbon'
 
 group :development, :test do
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module SirTrevorBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << File.expand_path('../../node_modules/', __FILE__)
   end
 end
