cb77c58b7971a699a1b76732c90dea4098e8de39
Lukasz Sagol <lukasz@sagol.pl>
2015-03-26 16:02:06 +0000

    Added SirTrevor to form field

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,14 @@
 var SirTrevor = require('sir-trevor');
+var $ = require('jquery');
+var area = document.querySelector('.sir-trevor-area');
 
-console.log(SirTrevor);
+if (area) {
+  new SirTrevor.Editor({
+    el: $(area),
+    config: {
+      debug: true,
+      scribeDebug: true
+    },
+    blockTypes: ["Heading", "Text", "List", "Quote", "Image", "Video", "Tweet"]
+  });
+}
